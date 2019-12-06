e1ba4daebff688ca83fd4081b3b96cbc451fb452
minhyungyook <yookkhun@gmail.com>
2019-12-06 15:44:48 +0900

    Vanilla Javascript Project

--- clock.js
+++ clock.js
@@ -1,6 +1,6 @@
 const clockContainer = document.querySelector(".js-clock");
 const clockTitle = clockContainer.querySelector("h1");
-
+ 
 function getTime(){
     const date = new Date();
     const minutes = date.getMinutes();
